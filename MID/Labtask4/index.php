5381f93978c0305888d2a32bc7631c99a8d10e96
DardinaSaraNarisa <dardinanarisa28509@email.com>
2021-11-15 01:04:12 +0600

    labtask4

---
+++ MID/Labtask4/index.php
@@ -0,0 +1,27 @@
+<?php require_once "functions.php"; ?>
+<?php
+
+// if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+//     header("Location: dashboard.php");
+//     exit();
+// }
+
+?>
+
+<?php header_page("Public Home"); ?>
+
+<?php primary_menu(); ?>
+
+    <section class="main">
+
+        <?php
+            if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+                aside_menu();
+            }
+        ?>
+
+        <main class="main__content">
+            <h1>Welcome</h1>
+        </main>
+    </section>
+
